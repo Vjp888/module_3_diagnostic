17030dea071bc12d7a3165758db1d46981158ac8
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-05-13 10:59:54 -0600

    Updates API url to call only public stations that are electric and propane

--- app/services/nrel_developer_service.rb
+++ app/services/nrel_developer_service.rb
@@ -4,9 +4,8 @@ class NrelDeveloperService
   end
 
   def all_stations
-    conn = Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_API_KEY']}&location=#{@zipcode}")
+    conn = Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_API_KEY']}&location=#{@zipcode}&access=public&fuel_type=ELEC,LPG")
     response  = conn.get
     data = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
   end
 end
