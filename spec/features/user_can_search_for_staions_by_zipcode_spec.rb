983230c6fd8dd418b6db879babf643be9174090d
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-05-13 09:29:46 -0600

    Renames test for more basic call adds pry and preps figaro

--- spec/features/user_can_search_for_staions_by_zipcode_spec.rb
+++ spec/features/user_can_search_for_staions_by_zipcode_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'As a user', type: :feature do
   describe 'When I enter a zipode' do
-    it 'shows a list of stations for that zipcode' do
+    it 'shows the zipcode the user is looking for' do
       visit root_path
 
       fill_in 'q', with: '80206'
@@ -10,8 +10,6 @@ RSpec.describe 'As a user', type: :feature do
       click_on 'Locate'
 
       expect(page).to have_content('Stations for 80206')
-
-
     end
   end
 end
